39aefc5a64f60a6ba509e3686bc06e653c6129db
Analeah Real <142811266+analeah@users.noreply.github.com>
2024-08-06 00:15:18 -0400

    process videos locally
    - installed ffmpeg nodejs wrapper
    - installed fluent-ffmpeg types
    - added video processing code
    - added .mp4 files
    - made local request (thunder client)

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,12 +1,37 @@
 import express from "express";
+import ffmpeg from "fluent-ffmpeg";
 
 const app = express();
-const port = 3000;
+app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.post('/process-video', (req, res) => {
+  // Get path of the input video file from the request body
+  const inputFilePath = req.body.inputFilePath;
+  const outputFilePath = req.body.outputFilePath;
+
+  // Check if the file path is defined
+  if (!inputFilePath) {
+    return res.status(400).send('Bad Request: Missing input file path.');
+  }
+  if (!outputFilePath) {
+    return res.status(400).send('Bad Request: Missing output file path.');
+  }
+
+  // Create the ffmpeg command
+  ffmpeg(inputFilePath)
+    .outputOptions('-vf', 'scale=-1:360') // 360p
+    .on('end', () => {
+      console.log('Processing finished successfully');
+      res.status(200).send('Processing finished successfully.')
+    })
+    .on('error', (err) => {
+      console.log(`Error occurred: ${err.message}`);
+      res.status(500).send(`Internal Server Error: ${err.message}`)
+    })
+    .save(outputFilePath);
 });
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
